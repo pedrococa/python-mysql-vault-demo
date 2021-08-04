908f7e7058f7dd2a0a70d3c237508128e21cca13
pedrococa <pedro.coca@gmail.com>
2021-08-04 12:43:53 +0200

    Vault upgraded to 1.8.0 and license not autoloaded

--- 06_start_vault.sh
+++ 06_start_vault.sh
@@ -3,4 +3,4 @@
 docker stop vault-demo-vault
 docker rm vault-demo-vault
 #start Vault in dev mode on port 8200
-docker run --detach --name vault-demo-vault --network dev-network -p 8200:8200 hashicorp/vault-enterprise:1.7.2_ent server -dev -dev-root-token-id="root"
+docker run --detach --name vault-demo-vault --network dev-network -p 8200:8200 -e VAULT_LICENSE=$(cat ./license.txt) hashicorp/vault-enterprise:1.8.0_ent server -dev -dev-root-token-id="root"
